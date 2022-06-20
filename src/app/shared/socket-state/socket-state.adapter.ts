4e1ac8985b8e618255a8b9607af463d8e53d0335
Helard Gomez <helard.gomez@fromsolvers.com>
2022-06-19 19:50:28 -0500

    first commit

---
+++ src/app/shared/socket-state/socket-state.adapter.ts
@@ -0,0 +1,92 @@
+import { INestApplicationContext, WebSocketAdapter } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+import socketio, { ServerOptions } from 'socket.io';
+import { RedisPropagatorService } from '../redis-propagator/redis-propagator.service';
+
+import { SocketStateService } from './socket-state.service';
+
+interface TokenPayload {
+  readonly userId: string;
+}
+
+export interface AuthenticatedSocket extends socketio.Socket {
+  auth: TokenPayload;
+}
+
+export class SocketStateAdapter extends IoAdapter implements WebSocketAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter>;
+
+  public constructor(
+    private readonly app: INestApplicationContext,
+    private readonly socketStateService: SocketStateService,
+    private readonly redisPropagatorService: RedisPropagatorService,
+  ) {
+    super(app);
+  }
+
+  async connectToRedis(): Promise<void> {
+    const pubClient = createClient({
+      url: `redis://localhost:6379`,
+      password: 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81',
+    });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    this.adapterConstructor = createAdapter(pubClient, subClient);
+  }
+
+  createIOServer(port: number, options?: ServerOptions): any {
+    const server = super.createIOServer(port, options);
+    server.adapter(this.adapterConstructor);
+    return server;
+  }
+
+  public create(port: number, options = {}): socketio.Server {
+    const server = super.createIOServer(port, options);
+    this.redisPropagatorService.injectSocketServer(server);
+
+    server.use(async (socket: AuthenticatedSocket, next) => {
+      const token =
+        socket.handshake.query?.token ||
+        socket.handshake.headers?.authorization;
+
+      if (!token) {
+        socket.auth = null;
+
+        // not authenticated connection is still valid
+        // thus no error
+        return next();
+      }
+
+      try {
+        // fake auth
+        socket.auth = {
+          userId: '1234',
+        };
+
+        return next();
+      } catch (e) {
+        return next(e);
+      }
+    });
+
+    return server;
+  }
+
+  public bindClientConnect(server: socketio.Server, callback: Function): void {
+    server.on('connection', (socket: AuthenticatedSocket) => {
+      if (socket.auth) {
+        this.socketStateService.add(socket.auth.userId, socket);
+
+        socket.on('disconnect', () => {
+          this.socketStateService.remove(socket.auth.userId, socket);
+
+          socket.removeAllListeners('disconnect');
+        });
+      }
+
+      callback(socket);
+    });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { RedisPropagatorService } from './app/shared/redis-propagator/redis-propagator.service';
+import { SocketStateAdapter } from './app/shared/socket-state/socket-state.adapter';
+import { SocketStateService } from './app/shared/socket-state/socket-state.service';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  const socketStateService = app.get(SocketStateService);
+  const redisPropagatorService = app.get(RedisPropagatorService);
+
+  const adapter = new SocketStateAdapter(
+    app,
+    socketStateService,
+    redisPropagatorService,
+  );
+
+  await adapter.connectToRedis();
+
+  app.useWebSocketAdapter(adapter);
+
+  await app.listen(3000, () => {
+    console.log(`Listening on port 3000.`);
+  });
+}
+
+bootstrap();
